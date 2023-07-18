bbb8d03471020fb0ecb39a9dfece93724216f3a7
Aaron <awitthohn@yahoo.com>
2023-07-18 11:07:50 -0400

    added logic to handle user answers.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 const { createSVGFile } = require('./lib/svgWriter');
 
+
+//Prompts user for their new logo
 function promptUser() {
   inquirer
     .prompt([
@@ -30,4 +32,30 @@ function promptUser() {
         name: 'textColor',
         message: 'Enter the text color (keyword or hexadecimal number):',
       },
-    ])}
+    ]}
+    //takes answers and builds logo
+    .then((answers) => {
+        const { text, textColor, shape, shapeColor } = answers;
+  
+        let svgContent = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">`;
+  
+        svgContent += `<text x="150" y="100" fill="${textColor}" text-anchor="middle">${text}</text>`;
+
+
+        //Creates the shape of user logo
+        const shapeobject = {
+            circle: new Circle(),
+            triangle: new Triangle(),
+            square: new Square(),
+          }[shape] || (() => { throw new Error('Invalid shape'); })();
+           // Set shape color
+      shapeObject.setColor(shapeColor);
+
+      svgContent += shapeObject.render();
+
+      svgContent += '</svg>';
+
+      createSVGFile(svgContent);
+    })
+
+    promptUser();
